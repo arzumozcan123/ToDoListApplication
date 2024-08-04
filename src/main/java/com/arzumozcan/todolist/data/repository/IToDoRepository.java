2a0e80406530bb7b38bb1c0adcd1378cbe4a0ee0
arzumozcan123 <ozcanarzum2001@gmail.com>
2024-08-04 20:52:07 +0300

    last commit

--- src/main/java/com/arzumozcan/todolist/data/repository/IToDoRepository.java
+++ src/main/java/com/arzumozcan/todolist/data/repository/IToDoRepository.java
@@ -2,9 +2,10 @@ package com.arzumozcan.todolist.data.repository;
 
 import com.arzumozcan.todolist.data.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+@Repository
 public interface IToDoRepository extends JpaRepository<Todo, Long> {
 
     Optional<Todo> findByTaskName(String taskName);
